b50a6c5e9bbb8c98c66edad521e790e5d2a0f3dd
Liza <pristyahaliza10@gmail.com>
2026-01-18 18:31:09 +0700

    add pages, layout and components

---
+++ components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Notification = () => {
+    return (
+        <div className="h-12 font-bold bg-red-500 text-white">Notification</div>
+    );
+};
+
+export default Notification;
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SingleProductPage = () => {
+    return (
+        <div>Single Product Page</div>
+    );
+};
+
+export default SingleProductPage;
